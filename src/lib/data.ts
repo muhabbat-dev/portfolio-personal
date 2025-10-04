292c1b0a07f7f560343189a03e234fd73e15e631
Muhabbat <dev.mohib@gmail.com>
2025-10-04 17:32:39 +0200

    initialized

---
+++ src/lib/data.ts
@@ -0,0 +1,111 @@
+export const personalInfo = {
+  name: "Muhabbat Ali",
+  location: "Paris, France",
+  email: "muhabbat.ali@epita.fr",
+  github: "https://github.com/muhabbat-dev",
+  linkedin: "https://linkedin.com/in/muhabbat-ali",
+  profilePicture: "/profile.png",
+  heroDescription: `Hi! I'm a Full Stack Developer with 3+ years of experience, currently
+pursuing an MSc in Software Engineering at EPITA, France. I specialize in
+building scalable web applications using ReactJS, NextJS, NodeJS, and
+Laravel. I'm passionate about creating efficient and user-friendly
+solutions that make a difference.`,
+};
+
+export const workExperience = [
+  {
+    company: "Freelancer",
+    location: "Remote",
+    position: "Full Stack Developer",
+    period: "Jan 2020 - Present",
+    achievements: [
+      "Completed 10+ projects for clients worldwide.",
+      "Delivered 100+ programming related tasks.",
+      "Specialized in web development using ReactJS, NextJS, NodeJS, and Laravel.",
+      "Delivered high-quality, scalable, and maintainable code.",
+      "Collaborated with clients to understand their requirements and provide tailored solutions.",
+      "Implemented responsive designs to ensure optimal user experience across devices.",,
+    ],
+  }
+];
+
+export const education = [
+  {
+    institution: "EPITA",
+    location: "Paris, France",
+    degree: "MSc Computer Science",
+    period: "2025 - Current",
+    achievements: [
+      "Software Engineering Major",
+      "DevOps and Cloud Computing",
+      "Full Stack Development",
+      "Agile Methodologies",
+    ],
+  },
+];
+export const skills = {
+  programmingLanguages: [
+    "JavaScript",
+    "TypeScript",
+    "Python",
+    "PHP",
+    "Java",
+    "Dart"
+  ],
+  frontendDevelopment: [
+    "ReactJS",
+    "NextJS",
+    "Astro",
+    "TailwindCSS",
+    "Bootstrap",
+    "HTML",
+    "CSS",
+  ],
+  backendDevelopment: ["NodeJS", "Laravel", "ExpressJS", "NestJS"],
+  databaseAndStorage: ["PostgreSQL", "MySQL", "MongoDB"],
+  cloudAndDevOps: ["AWS", "GCP", "Docker", "Docker Compose", "Terraform", "Ansible", "CI/CD"],
+  toolsAndServices: [
+    "Git",
+    "GitHub",
+    "Elasticsearch",
+    "N8N",
+    "Kibana",
+    "TinaCMS",
+    "Strapi",
+    "Sanity",
+  ],
+};
+
+export const projects = [
+  {
+    title: "Towkay Exchange",
+    github: "https://github.com/muhabbat-dev",
+    description: [
+      "Markeplace Application for buying and selling",
+      "Post free ads, search and filter products.",
+      "Upload businesses, franchises, services and events",
+      "Prmotion of ads to reach more customers.",
+      "Chat with sellers directly from the app.",
+    ],
+  },
+  {
+    title: "LTApany",
+    github: "https://github.com/muhabbat-dev",
+    description: [
+      "Car lease marketplace",
+      "Transfer lease bwtween users",
+      "Search and filter cars",
+      "Upload your car lease",
+    ],
+  },
+];
+
+export const awards = [
+  {
+    name: "ReactJS Expert",
+    issuer: "The University of Faisalabad",
+    date: "March 2022",
+    type: "University Certificate",
+    position: "First Place",
+  }
+];
